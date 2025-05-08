4bcc60d463bd72665b93567787c95ba671199667
RokitG <pintosdsgn@gmail.com>
2025-05-08 07:52:50 +0700

    @app: Add Next.js configuration .ts file

--- apps/nextjs/next.config.ts
+++ apps/nextjs/next.config.ts
@@ -1,11 +1,11 @@
+import type { NextConfig } from "next";
 import { fileURLToPath } from "node:url";
 import createJiti from "jiti";
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
 createJiti(fileURLToPath(import.meta.url))("./src/env");
 
-/** @type {import("next").NextConfig} */
-const config = {
+const config: NextConfig = {
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: [
     "@acme/api",
