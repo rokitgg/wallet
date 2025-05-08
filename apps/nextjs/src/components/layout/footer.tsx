eda4c6886c7dd4b63788d06dd77db4b435d596a1
RokitG <pintosdsgn@gmail.com>
2025-05-08 07:48:23 +0700

    @app: Add Footer component to layout with navigation links

---
+++ apps/nextjs/src/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="absolute bottom-8 flex gap-6 text-sm text-gray-400">
+    <a href="/developers" className="hover:underline">Github</a>
+    <a href="/privacy" className="hover:underline">Privacy</a>
+    <a href="/terms" className="hover:underline">Terms</a>
+  </footer>
+  );
+}
